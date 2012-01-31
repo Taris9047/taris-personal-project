f1709ddbdd30e9a51035e895a6f593b3ddcac6e7
TalezShin <TalezShin@66e85ac4-eb49-57d4-ed3e-464d482720bb>
2012-01-31 22:14:08 +0000

    fixed Makefile and prevented warnings.

--- trunk/factorial/factorial.f
+++ trunk/factorial/factorial.f
@@ -1,15 +1,15 @@
       program FACTORIAL
 
 !  definition of variables
-      integer i, res, number
-      parameter( number=50000 )
+      integer*8 i, res, number
+      parameter( number=50 )
 
       res = 1
-      do i=2,number   ! this loop calculates the factorial
+      do i=2,number             ! this loop calculates the factorial
          res = res*i
       enddo
 
-      write(*,'("Factorial of ",I3," is ",I5)') 
-     +     number, res
+      write(*,'("Factorial of ",I10," is ",I30)') 
+     +   number, res
 
       end
