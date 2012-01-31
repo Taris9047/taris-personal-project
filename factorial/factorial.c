9933de29f57a294de87a6939e22c72be3a46f961
TalezShin <TalezShin@66e85ac4-eb49-57d4-ed3e-464d482720bb>
2012-01-31 21:47:58 +0000

    Large factorial(x!) calculation comparison added.

---
+++ factorial/factorial.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <gmp.h>
+#include <time.h>
+ 
+void factorial(mpz_t* f, unsigned int n)
+{
+    unsigned int i = 1;
+ 
+    for (i = 1; i <= n ; i++){
+        mpz_mul_si(*f, *f, i);
+    }
+}
+ 
+int main(int argc, char **argv)
+{
+    mpz_t facN;
+    unsigned int n = 50000;
+    clock_t start, end;
+    double runtime;
+ 
+    mpz_init(facN);
+    mpz_set_ui(facN, 1L);
+ 
+    start = clock();
+    factorial(&facN, n);
+    end = clock();
+ 
+    //printf("fac %u is ", n);
+    //mpz_out_str(stdout, 10, facN);
+    //printf("\n");
+ 
+    runtime = (double)(end-start) / CLOCKS_PER_SEC;
+    printf ("runtime is %f\n", runtime);
+    mpz_clear(facN);
+}
