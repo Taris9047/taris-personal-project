357c653c9596c417dcd718167b7f8efa4a9ecf1d
TalezShin <TalezShin@66e85ac4-eb49-57d4-ed3e-464d482720bb>
2012-02-02 23:35:03 +0000

    Currently, tuning the mpfun77 library code to figure 50000! is possible to compute or not.

--- trunk/factorial/factorial.f
+++ trunk/factorial/factorial.f
@@ -1,12 +1,12 @@
-cmp+ precision level 1000
+cmp+ precision level 250000
 cmp+ mixed mode fast
-cmp+ output precision 1000
-cmp+ scratch space 5000
+cmp+ output precision 200000
+cmp+ scratch space 150000
       program factorial
 cmp+ multip integer res
       integer*8 i, Num
       Num = 50000
-      res = 1
+      res = 1+0
 c
       do i=2,Num
          res = res*i
@@ -14,6 +14,6 @@ c
 c
       write(*,'("Factorial of ",I10," has been finished.")') 
      +   Num
-      write(6, *) res
+c      write(6, *) res
       stop
       end
