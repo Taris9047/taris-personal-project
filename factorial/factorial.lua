4413efb480b82292475a02298416c57ed3626784
TalezShin <TalezShin@66e85ac4-eb49-57d4-ed3e-464d482720bb>
2012-02-08 16:27:08 +0000

    50000! with lua has been successfully calculated within 1 minute and 30 seconds. Slowest so far, compared to Python which took 1.4 seconds at maximum. (Using late 2008 15 " MBP)

--- factorial/factorial.lua
+++ factorial/factorial.lua
@@ -1,8 +1,17 @@
-#!/usr/bin/env lua
-fact = 1
+-- bc package was derived from
+-- http://penlight.luaforge.net/packages/lbc.html
+--
+require"bc"
+
+bc.digits (250000)
+
+fact = bc.number(1)
 N = 50000
-for i=1,N do
-	fact = fact*i
+
+for i=2,N do
+   fact = bc.mul(fact, i)
 end
-print(fact)
+
+--print(fact)
 print(N,"! calculation is finished!")
+
