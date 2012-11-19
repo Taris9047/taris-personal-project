08a7282d53731fc87644873d0686c51d96de5c2b
TalezShin <TalezShin@66e85ac4-eb49-57d4-ed3e-464d482720bb>
2012-11-19 22:16:43 +0000

    Now Benchmark.py can display statistics and CPU information.

--- trunk/Benchmark/Benchmark.py
+++ trunk/Benchmark/Benchmark.py
@@ -7,31 +7,52 @@
 import time
 import sys
 import factor_lib as flib
+import platform as plf
+import cpuinfo as cpu
+
+# Saving up information
+operating_system = plf.platform()
+uname = plf.uname()
+System_Info = cpu.cpu_type(uname[0])+'\n'+uname[3]+"\n"
 
 # Running routines
-N = [1000, 5000, 10000, 50000, 100000, 500000]
+#N = [1000, 5000, 10000, 50000, 100000, 500000]
+N = [1000, 5000, 10000, 50000, 100000]
 print ("Calculation range: ",N)
 time_diff = [None] * len(N)
 factN = []
 factNdnc = []
 
+print (System_Info)
 print ("Calculating Generic Sequential Algorithm...")
 for i in range(len(N)):
 	startTime = time.clock()
 	factN = flib.factorial(N[i])
 	endTime = time.clock()
 	time_diff[i] = endTime-startTime
-	print ("%d!	has been finished in %.2f seconds."%(N[i], time_diff[i]))
+	print ("%d!	has been finished in %.6f seconds."%(N[i], time_diff[i]))
 
 sequential_performance = time_diff
+f = open("Sequential.txt", 'w')
+f.write(System_Info)
+f.write("N\tElipsed Time\n")
+for i in range(len(N)):
+	f.write(str(N[i])+"\t"+str(sequential_performance[i])+"\n")
+f.close()
 
 print (" ")
 print ("Calculating Divide and Conquer Algorithm...")
 for i in range(len(N)):
 	startTime = time.clock()
-	factNdnc = flib.dnc(N[i],N[i]/10)
+	factNdnc = flib.dnc(N[i],N[i]/100)
 	endTime = time.clock()
 	time_diff[i] = endTime-startTime
-	print ("%d!	has been finished in %.2f seconds."%(N[i], time_diff[i]))
+	print ("%d!	has been finished in %.6f seconds."%(N[i], time_diff[i]))
 
 dnc_performance = time_diff
+f = open("DNC.txt", 'w')
+f.write(System_Info)
+f.write("N\tElipsed Time\n")
+for i in range(len(N)):
+	f.write(str(N[i])+"\t"+str(dnc_performance[i])+"\n")
+f.close()
