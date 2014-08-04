0a70d82330bd1bfae775b4bfa8aaa19ddc6e6e3c
TalezShin <TalezShin@66e85ac4-eb49-57d4-ed3e-464d482720bb>
2014-08-04 21:11:02 +0000

    Physics on the way.

--- trunk/Monte_Carlo/Physics.cpp
+++ trunk/Monte_Carlo/Physics.cpp
@@ -4,7 +4,7 @@
 #include <stdexcept>
 #include <cstdlib>
 #include <ctime>
-#include "Molecule.hpp"
+#include <string>
 #include "Physics.hpp"
 
 #ifndef PHYSICS_CPP
@@ -24,9 +24,14 @@ void Physics::advance_time()
 	this->x_vel.push_back(0.0);
 	this->y_vel.push_back(0.0);
 
-	this->time_eclipsed += this->time_scale;
-	this->time_trace.push_back(time_eclipsed);
+	this->curr_object->set_location( \
+		this->x_loc.back(),this->y_loc.back());
+	this->curr_object->set_velocity( \
+		this->x_vel.back(),this->y_vel.back());
 
+	this->time_elapsed += this->time_scale;
+	this->time_trace.push_back(this->time_elapsed);
+	this->reflected_status.push_back(false);
 }
 
 void Physics::brownian_rect(float max_vel_x, float max_vel_y)
@@ -46,15 +51,126 @@ void Physics::brownian_rect(float max_vel_x, float max_vel_y)
 	this->x_vel.push_back((float)rand()/max_vel_x);
 	this->y_vel.push_back((float)rand()/max_vel_y);		
 
-	this->time_eclipsed += this->time_scale;	
-	this->time_trace.push_back(time_eclipsed);
+	this->curr_object->set_location( \
+		this->x_loc.back(),this->y_loc.back());
+	this->curr_object->set_velocity( \
+		this->x_vel.back(),this->y_vel.back());
+
+	this->time_elapsed += this->time_scale;	
+	this->time_trace.push_back(this->time_elapsed);
+	this->reflected_status.push_back(false);
 }
 
-void Physics::reflect_rect(float edge_x, float edge_y)
+// Detects the initial collision location and returns it as float, float vector.
+std_vec_f Physics::collision_rect(float edge_left, float edge_right, \
+		float edge_top, float edge_bottom, float time_segment)
+{
+	std_vec_f col_loc(2);
+	std_vec_f proj_loc(2);
+	std_vec_f curr_loc(2);
+	std_vec_f temp_loc(2);
+	float temp_time = 0.0;
+	float temp_time_sc = this->time_scale * 1E-5;
+
+	proj_loc = this->proj_loc_rect(time_segment);
+	curr_loc[0] = this->x_loc.back();
+	curr_loc[1] = this->y_loc.back();
+
+	temp_loc = curr_loc;
+	int i = 0;
+
+	do {
+		temp_loc = this->proj_loc_rect(temp_time);
+		temp_time = temp_time_sc*i;
+		
+		if (temp_loc[0] > edge_right) {
+			temp_loc[0] = edge_right;
+			break;
+		}
+		else if (temp_loc[0] < edge_left) {
+			temp_loc[0] = edge_left;
+			break;
+		}
+		else if (temp_loc[1] > edge_top) {
+			temp_loc[1] = edge_top;
+			break;
+		}
+		else if (temp_loc[1] < edge_bottom) {
+			temp_loc[1] = edge_bottom;
+			break;
+		}
+
+		i++;
+	} while (1);
+
+	col_loc = temp_loc;
+
+/*
+	float slope = (proj_loc[1]-curr_loc[1])/(proj_loc[0]-curr_loc[0]);
+	float Y_pass = curr_loc[1] - slope*curr_loc[0];
+
+	float y_intercept_left = edge_left*slope + Y_pass;
+	float y_intercept_right = edge_right*slope + Y_pass;
+	float x_intercept_top = (edge_top-Y_pass)/slope;
+	float x_intercept_bottom = (edge_bottom-Y_pass)/slope;
+*/
+
+	return col_loc;
+}
+
+// Returns projected location after a certain time.
+std_vec_f Physics::proj_loc_rect(float time_segment)
+{
+	std_vec_f proj_loc(2);
+
+	proj_loc[0] = \
+		this->x_loc.back()+this->x_vel.back()*time_segment;
+	proj_loc[1] = \
+		this->y_loc.back()+this->y_vel.back()*time_segment;
+
+	return proj_loc;
+}
+
+// Reporting status on the way.
+void Physics::print_status_rect()
+{
+	std_vec_f curr_loc(2);
+	std_vec_f curr_vel(2);
+
+	curr_loc[0] = this->x_loc.back();
+	curr_loc[1] = this->y_loc.back();
+	curr_vel[0] = this->x_vel.back();
+	curr_vel[1] = this->y_vel.back();
+
+	if (this->reflected_status.back() == true) {
+		cout << "** Reflected!! **" << endl;
+	}
+
+	cout << "Location: (" << curr_loc[0] << \
+		 "," << curr_loc[1] << ")" << endl;
+	cout << "Velocity: <" << curr_vel[0] << \
+		 "," << curr_vel[1] << ">" << endl;
+	cout << "Time elapsed: " << this->time_elapsed \
+		<< " sec." << endl;
+
+}
+
+// Prepare log file.
+void Physics::write_log_rect(std_str outfile_name)
 {
 
-	if (this->x_loc.back() > edge_x || 
-		this->y_loc.back() > edge_y) {
+}
+
+
+// Calculate reflecting molecule in a limited space.
+void Physics::reflect_rect(float edge_left, float edge_right, \
+		float edge_top, float edge_bottom)
+{
+
+	if (this->x_loc.back() > edge_right || \
+		this->x_loc.back() < edge_left || \
+		this->y_loc.back() > edge_top || \
+		this->y_loc.back() < edge_bottom) {
 		throw invalid_argument( \
 			"The trace already found at the outside of \
 			the boundary. Cannot proceed!!\n\n");
@@ -62,32 +178,142 @@ void Physics::reflect_rect(float edge_x, float edge_y)
 	}
 
 	float div_time = 0.0; // Must be smaller than time scale.
-	float div_time_left = time_scale - div_time;
-	float div_ratio;
+	float div_time_left = this->time_scale - div_time;
 
 	do {
 		// Dealing with X axis first
-		float next_x = this->x_loc.back()+\
-			this->x_vel.back()*div_time_left;
-		float next_y = this->y_loc.back()+\
-			this->y_vel.back()*div_time_left;
+		std_vec_f proj_loc(2);
+		proj_loc = this->proj_loc_rect(div_time_left);
+		float next_x = proj_loc[0];
+		float next_y = proj_loc[1];
 
-		if (next_x > edge_x || next_x < edge_x*(-1)) {
-			float offset_x;
-			float div_ratio_x;
-			if (next_x > edge_x) {
-				offset_x = next_x - edge_x;
-			}
-			else if (next_x < edge_x*(-1)) {
-				offset_x = edge_x*(-1) - next_x;
-			}
-			// continue this crap
+		std_vec_f collision_loc(2);
+		collision_loc = this->collision_rect( \
+			edge_left, edge_right, edge_top, edge_bottom, \
+			div_time_left);
+
+		if (collision_loc[0] == edge_left || \
+			collision_loc[0] == edge_right) {
 			
+			float offset_x = 0.0;
+			float time_elapsed_ref = 0.0;
+			float next_y_ref = this->y_vel.back(); 
+			float next_x_ref = this->x_vel.back(); 
+			
+			// Calculating how much time elapsed before 
+			// reflection from x direction.
+			if (collision_loc[0] > edge_right) {
+				offset_x = next_x - edge_right;
+				time_elapsed_ref = offset_x/this->x_vel.back();
+				next_x_ref = edge_right;
+			}
+			else if (collision_loc[0] < edge_left) {
+				offset_x = edge_left - next_x;
+				time_elapsed_ref = abs(offset_x)/this->x_vel.back();
+				next_x_ref = edge_left;
+			}
+
+			// Update y corresponding to x until hitting
+			// the wall.
+			next_y_ref = this->y_loc.back()+\
+				this->y_vel.back()*time_elapsed_ref;
+
+			// Update status to Molecule class.
+			this->x_loc.push_back(next_x_ref);
+			this->y_loc.push_back(next_y_ref);
+			this->x_vel.push_back(this->x_vel.back()*(-1));
+			this->y_vel.push_back(this->y_vel.back());
+			this->time_elapsed += time_elapsed_ref;
+			this->time_trace.push_back(this->time_elapsed);
+			this->reflected_status.push_back(true);
+			div_time += time_elapsed_ref;
+			div_time_left = this->time_scale - div_time;
 
 		}
 
-	} while (div_time <= time_scale);
+		else if (collision_loc[1] == edge_top || \
+			collision_loc[1] == edge_bottom) {
+			float offset_y = 0.0;
+			float time_elapsed_ref = 0.0;
+			float next_y_ref = this->y_vel.back(); 
+			float next_x_ref = this->x_vel.back(); 
+			
+			// Calculating how much time elapsed before 
+			// reflection from x direction.
+			if (collision_loc[1] > edge_top) {
+				offset_y = next_x - edge_top;
+				time_elapsed_ref = offset_y/this->y_vel.back();
+				next_y_ref = edge_top;
+			}
+			else if (collision_loc[1] < edge_bottom) {
+				offset_y = edge_bottom - next_y;
+				time_elapsed_ref = abs(offset_y)/this->y_vel.back();
+				next_y_ref = edge_bottom;
+			}
 
+			// Update y corresponding to x until hitting
+			// the wall.
+			next_x_ref = this->x_loc.back()+\
+				this->x_vel.back()*time_elapsed_ref;
+
+			// Update status to Molecule class.
+			this->x_loc.push_back(next_x_ref);
+			this->y_loc.push_back(next_y_ref);
+			this->x_vel.push_back(this->x_vel.back()*(-1));
+			this->y_vel.push_back(this->y_vel.back());
+			this->time_elapsed += time_elapsed_ref;
+			this->time_trace.push_back(this->time_elapsed);
+			this->reflected_status.push_back(true);
+			div_time += time_elapsed_ref;
+			div_time_left = this->time_scale - div_time;
+		}
+
+		// When velocity vector head directly to the very tips.
+		// 
+		else if ( (collision_loc[0] == edge_left && \
+			collision_loc[1] == edge_bottom) || \
+			(collision_loc[0] == edge_left && \
+			collision_loc[1] == edge_top) || \
+			(collision_loc[0] == edge_right && \
+			collision_loc[1] == edge_bottom) || \
+			(collision_loc[0] == edge_right && \
+			collision_loc[1] == edge_top) ) {
+
+			float offset_x = 0.0;
+			float time_elapsed_ref = 0.0;
+			float next_y_ref = this->y_vel.back(); 
+			float next_x_ref = this->x_vel.back(); 
+			
+			// Calculating how much time elapsed before 
+			// reflection from x direction.
+			if (collision_loc[0] > edge_right) {
+				offset_x = next_x - edge_right;
+				time_elapsed_ref = offset_x/this->x_vel.back();
+				next_x_ref = edge_right;
+			}
+			else if (collision_loc[0] < edge_left) {
+				offset_x = edge_left - next_x;
+				time_elapsed_ref = abs(offset_x)/this->x_vel.back();
+				next_x_ref = edge_left;
+			}
+
+			// Update y corresponding to x until hitting
+			// the wall.
+			next_y_ref = this->y_loc.back()+\
+				this->y_vel.back()*time_elapsed_ref;
+
+			// Update status to Molecule class.
+			this->x_loc.push_back(next_x_ref);
+			this->y_loc.push_back(next_y_ref);
+			this->x_vel.push_back(this->x_vel.back()*(-1));
+			this->y_vel.push_back(this->y_vel.back()*(-1));
+			this->time_elapsed += time_elapsed_ref;
+			this->time_trace.push_back(this->time_elapsed);
+			this->reflected_status.push_back(true);
+			div_time += time_elapsed_ref;
+			div_time_left = this->time_scale - div_time;		
+		}
+	} while (div_time <= time_scale);
 }
 
 
@@ -96,19 +322,22 @@ void Physics::reflect_rect(float edge_x, float edge_y)
 
 Physics::Physics()
 {
-	this->time_eclipsed = 0.0;
+	this->time_elapsed = 0.0;
 	this->time_scale = 1.0;
 	this->curr_object = NULL;
 
 	this->obj_mass = 0.0;
 
-	this->time_trace.push_back(0);
+	this->time_trace.push_back(this->time_elapsed);
+
+	this->reflected_status.push_back(false);
+
 }
 
 Physics::Physics(Molecule* Thing)
 {
-	this->time_eclipsed = 0.0;
-	this->time_scale = 1.0;
+	this->time_elapsed = 0.0;
+	this->time_scale = 1.0; // Default 1 sec time interval.
 	this->curr_object = Thing;
 
 	vector<float> obj_info(5);
@@ -119,12 +348,14 @@ Physics::Physics(Molecule* Thing)
 	this->y_vel.push_back(obj_info[3]);
 	this->obj_mass = obj_info[4];
 
-	this->time_trace.push_back(0);
+	this->time_trace.push_back(this->time_elapsed);
+
+	this->reflected_status.push_back(false);
 }
 
-Physics::Physics(float time_ec, float time_sc, Molecule* Thing)
+Physics::Physics(float time_el, float time_sc, Molecule* Thing)
 {
-	this->time_eclipsed = time_ec;
+	this->time_elapsed = time_el;
 	this->time_scale = time_sc;
 	this->curr_object = Thing;
 
@@ -136,8 +367,9 @@ Physics::Physics(float time_ec, float time_sc, Molecule* Thing)
 	this->y_vel.push_back(obj_info[3]);
 	this->obj_mass = obj_info[4];
 
-	this->time_trace.push_back(0);
+	this->time_trace.push_back(this->time_elapsed);
 
+	this->reflected_status.push_back(false);
 }
 
 Physics::~Physics()
@@ -146,8 +378,11 @@ Physics::~Physics()
 	this->y_loc.clear();
 	this->x_vel.clear();
 	this->y_vel.clear();
+	this->curr_object = NULL;
 
 	this->time_trace.clear();
+
+	this->reflected_status.clear();
 }
 
 #endif
--- trunk/Monte_Carlo/Physics.hpp
+++ trunk/Monte_Carlo/Physics.hpp
@@ -3,7 +3,10 @@
 #ifndef PHYSICS_HPP
 #define PHYSICS_HPP
 
-using namespace std;
+
+typedef std::vector<float> std_vec_f;
+typedef std::vector<bool> std_vec_b;
+typedef std::string std_str;
 
 // Physics implementation
 
@@ -13,33 +16,42 @@ class Physics: public Molecule
 {
 private:
 	// Setting up time and time scale.
-	float time_eclipsed;
+	float time_elapsed;
 	float time_scale;
 	// current object: Molecule (pointer)
 	Molecule* curr_object;
 
 	// Logging trace of the object.
-	vector<float> x_loc;
-	vector<float> y_loc;
-	vector<float> x_vel;
-	vector<float> y_vel;
-	vector<float> time_trace;
+	std_vec_f x_loc;
+	std_vec_f y_loc;
+	std_vec_f x_vel;
+	std_vec_f y_vel;
+	std_vec_b reflected_status;
+	std_vec_f time_trace;
 
 	// Add more constants if other coordinate system has used.
 
 	float obj_mass;
 
+	std_vec_f collision_rect(float edge_left, float edge_right, \
+		float edge_top, float edge_bottom, float time_segment); // Detects the first impact location and returns it.
+	std_vec_f proj_loc_rect(float time_segment); // returns coordinate after a certain time segment.
+
 public:
 	// reflecting in rectangular system
-	void reflect_rect(float edge_x, float edge_y);
+	void reflect_rect(float edge_left, float edge_right, \
+		float edge_top, float edge_bottom);
 	void brownian_rect(float max_vel_x, float max_vel_y);
-
 	void advance_time();
+
+	// Report Status
+	void print_status_rect();
+	void write_log_rect(std_str outfile_name);
 
 	// Constructures and Destructors
 	Physics();
 	Physics(Molecule* Thing);
-	Physics(float time_ec, float time_sc, Molecule* Thing);
+	Physics(float time_el, float time_sc, Molecule* Thing);
 	~Physics();
 
 };
