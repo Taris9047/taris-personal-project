f4f2917ec15bdd97ddc694e58721b6274f5b2f3a
TalezShin <TalezShin@66e85ac4-eb49-57d4-ed3e-464d482720bb>
2014-08-15 21:00:05 +0000

    Populated Random class a bit.

--- trunk/Monte_Carlo/Random.hpp
+++ trunk/Monte_Carlo/Random.hpp
@@ -1,13 +1,29 @@
 #ifndef RANDOM_HPP
 #define RANDOM_HPP
 
+typedef unsigned int unint;
+
 class Random
 {
+private:
+	double beta_f(double x, double y);
+	double gamma_f(double t);
+	unint factorial(unint n); // Do not use input over 100!!! it will crash!
+
 public:
+	// For... you know... redandancy. A seed generator, if needed.
 	void Seed_Rand();
 
+	// Uniform distribution
 	double uniform(double min, double max);
+	// Gaussian distribution (Normal distribution)
 	double gaussian(double mean, double sigma, double amplitude);
+	// Beta distribution (amplitude is always 1)
+	double beta(double a, double b);
+	// Binomial distribution
+	double binomial(unint n, unint p);
+	// Chi-square distribution
+	double chi_square(double k);
 
 	Random();
 };
