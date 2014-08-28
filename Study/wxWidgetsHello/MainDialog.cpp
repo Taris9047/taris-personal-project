08b94060503b4f554c5ec35f67d998e906bd8d38
TalezShin <TalezShin@66e85ac4-eb49-57d4-ed3e-464d482720bb>
2014-08-28 14:59:40 +0000

    wxWidgets CPP hello world

--- Study/wxWidgetsHello/MainDialog.cpp
+++ Study/wxWidgetsHello/MainDialog.cpp
@@ -9,3 +9,13 @@ MainDialog::~MainDialog()
 {
 }
 
+void MainDialog::OnIDEXITClicked(wxCommandEvent& event)
+{
+	exit(0);
+}
+void MainDialog::OnIDHELLOClicked(wxCommandEvent& event)
+{
+	m_staticTextHello->SetLabel("Hello, World!!");
+	m_textCtrlHello->Remove(0, m_textCtrlHello->GetLastPosition());
+	m_textCtrlHello->WriteText("Hello, World!!!");
+}
