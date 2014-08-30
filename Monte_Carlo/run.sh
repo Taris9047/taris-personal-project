5cda2cb86864ed6e05a1a86932d35bb2ff24fb61
TalezShin <TalezShin@66e85ac4-eb49-57d4-ed3e-464d482720bb>
2014-08-30 04:43:52 +0000

    revised run.sh again. Now detects OS type.

--- Monte_Carlo/run.sh
+++ Monte_Carlo/run.sh
@@ -1,7 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 
-make
+echo "Trying to compile... if anything changed in CPP code."
+make -j 6
+
+echo "Running the brownian movement simulator with arguments."
 ./brownian $1 $2 $3
+
+echo "Plotting the log with gnuplot."
 gnuplot ./plot.gp 
+
+echo "Converting the .eps to .pdf"
 convert -density 300 ./trace.eps ./trace.pdf
-open ./trace.pdf
+
+echo "Opening the .pdf file"
+if [ $OSTYPE == "linux-gnu" ]; then
+    evince ./trace.pdf
+elif [ $OSTYPE == "darwin" ]; then
+    open ./trace.pdf
+elif [ $OSTYPE == "cygwin" ]; then
+    xpdf ./trace.pdf
+else
+    echo "Open ./trace.pdf to watch the show!!"
+fi
+
