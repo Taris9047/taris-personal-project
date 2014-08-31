80de7f4d25e4faab50ba698b5fcc49e67b8a365e
TalezShin <TalezShin@66e85ac4-eb49-57d4-ed3e-464d482720bb>
2014-08-31 11:35:03 +0000

    Added string to data converters.

---
+++ trunk/Monte_Carlo/Converters.hpp
@@ -0,0 +1,28 @@
+#ifndef CONVERTERS_HPP
+#define CONVERTERS_HPP
+
+//#include "data_types.hpp"
+#include <cstdlib>
+#include <sstream>
+#include <string>
+#include <vector>
+
+namespace Converters
+{
+	const char* numtostr(long double input);
+	const char* numtostr(double input);
+	const char* numtostr(float input);
+	const char* numtostr(int input);
+
+
+	const char* btostr(bool input);
+	const char* btoyesno(bool input);
+
+	double strtod(const char* str);
+	float strtof(const char* str);
+	int strtoi(const char* str);
+	bool strtob(const char* str);
+
+};
+
+#endif
