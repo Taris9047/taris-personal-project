59524b8fbbad50d65d8403cf40dbc3504cb372ac
TalezShin <TalezShin@66e85ac4-eb49-57d4-ed3e-464d482720bb>
2014-09-12 02:49:33 +0000

    Oh well, it's not easy.

--- bignum/test.c
+++ bignum/test.c
@@ -7,8 +7,14 @@ int main()
 {
 	bnt testnum = bignum(-1231234454);
 	bnt justnum = "1231123234324143513241432";
+	bnt result;
 	printf ("Converted from integer: %s\n", testnum);
 	printf ("Direct Input: %s\n", justnum);
+
+	printf ("Performing addition. Prolly end up with stack overflow!!\n");
+	result = bignum_add(testnum, justnum);
+	printf ("Added Value: %s", result);
+	printf ("\n");
 
 	printf ("Press any key to continue.\n");
 	getchar();
