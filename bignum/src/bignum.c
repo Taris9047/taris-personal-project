68238806ec8fcdad5b3a2aac8a927793089ca03c
TalezShin <TalezShin@66e85ac4-eb49-57d4-ed3e-464d482720bb>
2014-09-14 00:57:14 +0000

    Grrrr, I HATE MEMORY ALLOCATION!!! Switch to C++??

--- bignum/src/bignum.c
+++ bignum/src/bignum.c
@@ -13,44 +13,55 @@ bnt bignum_add(bnt a, bnt b)
 	int neg_polarity_a; // 0 for positive, 1 for negative
 	int neg_polarity_b; // 0 for positive, 1 for negative
 
-	// Dealing with negative signs
 	bnt tmp_a;
-	bnt tmp_b;	
+	bnt tmp_b;
+	// Dealing with negative signs
 	if (a[0] == '-') {
 		neg_polarity_a = 1;
-
-		bnt tmp_a = (bnt)malloc(sizeof(char)*(strlen(a)-1));
-		tmp_a = bntcrop(a, 0);
+		tmp_a = (bnt)malloc(sizeof(char)*(strlen(a)-1));
+		if (tmp_a != NULL) {
+			tmp_a = bntcrop(a, 0);
+		}
 		//bnt a = (bnt)malloc(sizeof(char)*strlen(tmp_a));
 		//a = tmp_a;
 		//printf("a (after - deletion) is: %s\n", a);
 	}
 	else {
 		tmp_a = (bnt)malloc(sizeof(char)*(strlen(a)));
-		tmp_a = a;
+		if (tmp_a != NULL) {
+			tmp_a = a;
+		}
 		neg_polarity_a = 0;
 	}
 
 	if (b[0] == '-') {
 		neg_polarity_b = 1;
-
-		bnt tmp_b = (bnt)malloc(sizeof(char)*(strlen(b)-1));
-		tmp_b = bntcrop(b, 0);
+		tmp_b = (bnt)malloc(sizeof(char)*(strlen(b)-1));
+		if (tmp_b != NULL) {
+			tmp_b = bntcrop(b, 0);
+		}
+	
 		//bnt b = (bnt)malloc(sizeof(char)*strlen(tmp_b));
 		//b = tmp_b;
 		//printf("b (after - deletion) is: %s\n", b);
 	}
 	else {
 		tmp_b = (bnt)malloc(sizeof(char)*(strlen(b)));
-		tmp_b = b;
+		if (tmp_b != NULL) {
+			tmp_b = b;
+		}
 		neg_polarity_b = 0;
 	}
 
-	printf("bntadd, a and b: %s, %s\n", a, b);
-	printf("bntadd, tmp_a and tmp_b: %s, %s\n", tmp_a, tmp_b);
+	free(a);
+	free(b);
+
+	//printf("bntadd, a and b: %s, %s\n", a, b);
+	//printf("bntadd, tmp_a and tmp_b: %s, %s\n", tmp_a, tmp_b);
 
 	if (neg_polarity_a == 0 && neg_polarity_b == 0) {
-		printf("bntadd, polarity + n +, a, b: %s, %s\n", a, b);
+		printf("bntadd, polarity + n +, a, b: %s, %s\n", tmp_a, tmp_b);
+		printf("bntadd, return %s\n", _add(tmp_a, tmp_b));
 		return _add(tmp_a, tmp_b);
 	}
 	else if (neg_polarity_a == 1 && neg_polarity_b == 0) {
@@ -60,7 +71,8 @@ bnt bignum_add(bnt a, bnt b)
 		// pass
 	}
 	else if (neg_polarity_a == 1 && neg_polarity_b == 1) {
-		printf("bntadd, polarity - n -, a, b: %s, %s\n", a, b);
+		printf("bntadd, polarity - n -, a, b: %s, %s\n", tmp_a, tmp_b);
+		printf("bntadd, return %s\n", _add(tmp_a, tmp_b));
 		return bntpush(_add(tmp_a, tmp_b), '-');
 	}
 
@@ -73,16 +85,18 @@ bnt _add(bnt a, bnt b)
 {
 	bnt ret;
 	
-	printf("_add, a, b: %s, %s\n", a, b);
+	//printf("_add, a, b: %s, %s\n", a, b);
 
 	// Assign longer array to ret
 	if (strlen(a) >= strlen (b)) {
 		ret = (bnt)malloc(strlen(a)*sizeof(char));
-		ret = a;
+		if (ret != NULL)
+			ret = a;
 	}
 	else {
 		ret = (bnt)malloc(strlen(b)*sizeof(char));
-		ret = b;
+		if (ret != NULL)
+			ret = b;
 	}
 
 	unsigned int an;
@@ -90,8 +104,8 @@ bnt _add(bnt a, bnt b)
 	unsigned int cn = 0;
 	unsigned int cn_n;
 	unsigned int rn = 0;
-	int i = strlen(ret)-1;
 
+	int i = strlen(ret)-1;
 	int i_a = strlen(a)-1;
 	int i_b = strlen(b)-1;
 	unsigned int ext_r = 0;
@@ -137,8 +151,8 @@ bnt _add(bnt a, bnt b)
 //	free(a); free(b);
 
 	if (ext_r != 0) {
-		printf("_add, ret: %s\n", ret);
-		printf("_add, ext_r: %u\n", ext_r);
+		//printf("_add, ret: %s\n", ret);
+		//printf("_add, ext_r: %u\n", ext_r);
 		return bntpush(ret, itoc(ext_r));
 	}
 	else {
@@ -161,14 +175,20 @@ bnt bignum(int num)
 		digits = count_ifs(num)+1;
 
 	ret = (char*)malloc(digits*sizeof(char));
-	sprintf(ret, "%d", num);
+	if (ret != NULL) {
+		sprintf(ret, "%d", num);
+	}
+	
 	return ret;
 }
 
 bnt bignum_constchar(const char* number)
 {
 	bnt ret = (bnt)malloc((unsigned)strlen(number)*sizeof(char));
-	strcpy(ret, number);
+	if (ret != NULL) {
+		strcpy(ret, number);
+	}
+		
 	return ret;
 }
 
