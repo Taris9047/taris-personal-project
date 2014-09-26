8ebe8b13fbaa7030b02b82c5f7d886cff6a89c1d
TalezShin <TalezShin@66e85ac4-eb49-57d4-ed3e-464d482720bb>
2014-09-26 17:08:03 +0000

    working on a bit more sophisticated bignum library...

--- bignum/src/bignum_int.h
+++ bignum/src/bignum_int.h
@@ -4,10 +4,15 @@
 #include "data_type_bignum.h"
 #include <stdlib.h>
 #include "DataStr/llist.h"
+#include "utils.h"
 
 typedef struct bignum_int {
 	BOOL sign;
 	SLIST num_list;
 } bignum_int;
+
+// Constructors and Destructors
+bignum_int bnint(int num);
+
 
 #endif
