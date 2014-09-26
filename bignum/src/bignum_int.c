e597601a53afd94d7b9fac13ba34e2c9ce5a1f9e
TalezShin <TalezShin@66e85ac4-eb49-57d4-ed3e-464d482720bb>
2014-09-26 15:51:33 +0000

    Re-arranging stuff

---
+++ bignum/src/bignum_int.c
@@ -0,0 +1,6 @@
+#ifndef BIGNUM_INT_C
+#define BIGNUM_INT_C
+
+#include "bignum_int.h"
+
+#endif
---
+++ bignum/src/data_type_bignum.h
@@ -0,0 +1,16 @@
+#ifndef DATA_TYPE_BIGNUM_H
+#define DATA_TYPE_BIGNUM_H
+
+/*
+typedef char* bnt;
+typedef const char* cbnt;
+typedef unsigned int unint;
+typedef unsigned long ulong;
+typedef unsigned long long ullong;
+typedef int BOOL;
+
+#define TRUE 1
+#define FALSE 0
+*/
+
+#endif
