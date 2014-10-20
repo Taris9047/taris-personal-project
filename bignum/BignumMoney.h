3ea2cfd047a85128e958fd2b9da522364f58bf1e
TalezShin <TalezShin@66e85ac4-eb49-57d4-ed3e-464d482720bb>
2014-10-20 16:17:55 +0000

    added BignumMoney

---
+++ bignum/BignumMoney.h
@@ -0,0 +1,29 @@
+#ifndef BIGNUMMONEY_H
+#define BIGNUMMONEY_H
+
+#include "BignumInt.h"
+
+class BignumMoney : public virtual BignumInt
+{
+private:
+	SVI num_list;
+	bool sign;
+	std::string currency;
+	int comma_separation;
+
+public:
+	STR str(); // returns money value as string.
+	void SetNum(int num);
+	void SetSign(bool new_sign);
+	void SetCurrency(STR currency_str);
+	void SetComma(unsigned int commaloc);
+
+	// Constructors and destructors
+	BignumMoney();
+	BignumMoney(int num, STR currency_str);
+	BignumMoney(char* num, STR currency_str);
+	BignumMoney(const char* num, STR currency_str);
+
+};
+
+#endif
