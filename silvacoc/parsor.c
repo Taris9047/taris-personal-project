9889acfaf9537bb3b615b221585cf42f4e3fbccb
Taris9047 <talezshin@gmail.com>
2015-09-13 12:23:47 -0500

    silvacoc
    Working on silvacoc... just for fun.

---
+++ silvacoc/parsor.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "parsor.h"
+
+#ifndef __PARSOR_C__
+#define __PARSOR_C__
+
+
+
+#endif
---
+++ silvacoc/parsor.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifndef __PARSOR_H__
+#define __PARSOR_H__
+
+
+
+#endif
