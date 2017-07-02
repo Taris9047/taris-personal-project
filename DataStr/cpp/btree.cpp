daea52ea91aa0ede227ecc5de2254f913c2b29f4
Taylor Shin <talezshin@gmail.com>
2017-07-02 12:49:03 -0500

    some files

---
+++ DataStr/cpp/btree.cpp
@@ -0,0 +1,78 @@
+/*****************************************
+
+	Another attempt to implement
+	a data structure with C++11 (STL)
+
+	Binary Tree with arbitrary pointer type.
+
+	Implementation file.
+
+	Written by Taylor Shin
+	June 22th 2017
+
+******************************************/
+
+#include "btree.hpp"
+
+/*****************************************
+  BTNode stuffs
+******************************************/
+/* The real definitions for BTNode */
+struct BTree::btnode {
+private:
+  BTNode left;
+  BTNode right;
+  bt_data_t data;
+  uint64_t key;
+
+public:
+  /* Some methods */
+  BTNode GetLeft() const;
+  void SetLeft(const BTNode& o) { left = o; }
+  BTNode GetRight() const;
+  void SetRight(const BTNode& o) { right = o; }
+  uint64_t GetKey() const;
+  void SetKey(uint64_t o_k) { key = o_k; }
+  bt_data_t GetData() const;
+  void SetData(bt_data_t d) { data = d; }
+
+public:
+  /* Constructors and destructors */
+  btnode() : left(nullptr), right(nullptr), data(nullptr) {}
+  btnode(const& btnode) : btnode()
+  {
+    left = btnode.left;
+    right = btnode.right;
+    data = btnode.data;
+    key = btnode.key;
+  }
+  ~btnode() {}
+};
+
+/*****************************************
+  BTree - Methods (Manipulation)
+******************************************/
+void BTree::Add(bt_data_t d, uint64_t key)
+{
+  #pragma omp critical
+  {
+
+  }
+}
+
+
+/*****************************************
+  BTree - Access and utility methods
+******************************************/
+
+
+/*****************************************
+  BTree - Constructors and Destructors
+******************************************/
+BTree::BTree() : root(nullptr), key_list(nullptr), size(0)
+{}
+
+BTree::BTree(const BTree& other) : BTree()
+{
+
+}
---
+++ DataStr/cpp/btree.hpp
@@ -0,0 +1,63 @@
+/*****************************************
+
+	Another attempt to implement
+	a data structure with C++11 (STL)
+
+	Binary Tree with arbitrary pointer type.
+
+	Header file.
+
+	Written by Taylor Shin
+	June 22th 2017
+
+******************************************/
+
+#ifndef CPP_DATA_STRUCTURE_IMPLEMENTATION_BINARY_TREE_HEADER
+#define CPP_DATA_STRUCTURE_IMPLEMENTATION_BINARY_TREE_HEADER
+
+#include <memory>
+#include <vector>
+#include <cstdlib>
+#include <cstdint>
+
+#include "list.hpp"
+
+/* data type */
+typedef void* bt_data_t;
+
+/* Binary tree class */
+class BTree
+{
+public:
+  /* Constructors and destructors */
+  BTree();
+  BTree(const BTree&);
+  BTree& operator=(const BTree&);
+  ~BTree() noexcept;
+
+  /* Node struct */
+  struct btnode;
+  typedef btnode* BTNode;
+
+  /* Manipulation methods */
+  void Add(bt_data_t, uint64_t key);
+  void Remove(uint64_t key);
+
+  /* Access methods */
+  bt_data_t Get(uint64_t key);
+
+  /* Some utils */
+  bool IsEmpty();
+  uint64_t Size();
+
+private:
+  BTNode root;
+  uint64_t size;
+  List* key_list;
+
+  bool Find(bt_data_t);
+
+};
+
+
+#endif /* Include guard */
