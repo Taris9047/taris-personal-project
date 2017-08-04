ee4138e8bce748619fa4ce0e0a8d1d06ae6c2636
Taylor Shin <talezshin@gmail.com>
2017-08-04 15:42:43 -0500

    break:
    implemented QList version but this is strange. Pthread version fails but
    OpenMP actually works fine!!
    
    In fact, this implementation defines a large chunk of QList or regular
    array before working with malloc. So, It may not implement real
    situation: dynamic QList manipulation.
    
    We can try this later.

--- break_test/break.cpp
+++ break_test/break.cpp
@@ -1,3 +1,11 @@
+/************************************
+
+  Some crappy experiment code on
+  malloc/QList crash
+	
+	Implementation file
+  
+*************************************/
 
 #include "break.hpp"
 
@@ -9,14 +17,18 @@ int main (int argc, char* argv[])
 	if (argc > 1) n_threads = (int32_t)atoi(argv[1]);
 	int32_t ary_len = n_threads*ARY_LEN_MULTI;
 
+	/* Trying random access to random spots in an array */
+#if defined(QT_MODE)
+	QList<scNode*> ql_dummy_array;
+	ql_dummy_array.reserve(ary_len);
+	qt_break_malloc(n_threads, ql_dummy_array);
+#else
 	scNode** dummy_array = (scNode**)calloc(sizeof(scNode*),ary_len);
 	if (!dummy_array) exit(-1);
-	
-	/* Trying random access to random spots in an array */
 	break_malloc(n_threads, dummy_array);
-	
 	/* It won't reach here but... */
-  free(dummy_array);
+	free(dummy_array);
+#endif
 
 	return 0;
 }
--- break_test/break.hpp
+++ break_test/break.hpp
@@ -12,11 +12,20 @@
 #include <iostream>
 #include <cstdint>
 
+/* 
+  Comment this macro if you want to run 
+  a typical array version
+  instead of QList.
+*/
+#define QT_MODE
+
 #if defined(_OPENMP)
 #  include <omp.h>
 #else
 #  include <pthread.h>
 #endif
+
+#include <QList>
 
 #define N_THREADS 100
 #define ARY_LEN_MULTI 10
@@ -70,11 +79,44 @@ void break_malloc(size_t n_threads, scNode** dummy_array)
   } /* #pragma omp parallel num_threads(n_threads) */
 }
 
-#else
-/* pthread version */
-extern "C" {
-#include <stdbool.h>
+void qt_break_malloc(size_t n_threads, QList<scNode*>& dummy_array)
+{
+  omp_set_dynamic(0);
+  #pragma omp parallel num_threads(n_threads)
+  {
+    auto id = omp_get_thread_num();
+    
+    #pragma omp critical
+    {
+    std::cout << "Thread[" << id << "] is working with indices: ";
+    for (auto k=0; k<ARY_LEN_MULTI; ++k) {
+      std::cout << id*ARY_LEN_MULTI+k;
+      if (k==ARY_LEN_MULTI-1) continue;
+      else std::cout << ",";
+    }
+    std::cout << std::endl;
+    }
+    
+    while (true) {
+      for (auto i=0; i<ARY_LEN_MULTI; ++i) {
+        auto index = id*ARY_LEN_MULTI+i;
+        if (dummy_array[index]) {
+          if (rand_coin_toss()) {
+            free(dummy_array[index]);
+            dummy_array[index] = (scNode*)malloc(sizeof(scNode));
+          }
+          else continue;
+        }
+        else dummy_array[index] = (scNode*)malloc(sizeof(scNode));
+      } /* for (auto i=0; i<ARY_LEN_MULTI; ++i) */
+    }
+  } /* #pragma omp parallel num_threads(n_threads) */
+}
 
+#else /* #if defined(_OPENMP) */
+
+
+/* pthread version */
 pthread_mutex_t text_print_mutex;
 
 /* some struct for worker parameter */
@@ -126,7 +168,7 @@ void break_malloc(size_t n_threads, scNode** dummy_array)
   
   /* Let's call the workers */
   int rc;
-  int64_t t;
+  uint64_t t;
   pthread_t threads[n_threads];
   
   pthread_mutex_init(&text_print_mutex, NULL);
@@ -146,7 +188,83 @@ void break_malloc(size_t n_threads, scNode** dummy_array)
   pthread_exit(NULL);
 }
 
-} /* extern "C" */
+
+
+
+
+
+/* Qt version */
+
+typedef struct _qt_worker_param {
+  int64_t id;
+  QList<scNode*>* array;
+} QtWParam;
+
+
+void* qt_worker(void* params)
+{
+  int64_t id = ((QtWParam*)params)->id;
+  QList<scNode*>* dummy_array = ((QtWParam*)params)->array;
+
+  pthread_mutex_lock(&text_print_mutex);
+  printf("Thread[%lu] is working with indices: ", id);
+  for (int i=0; i<ARY_LEN_MULTI; ++i) {
+    printf("%lu", id*ARY_LEN_MULTI+i);
+    if (i==ARY_LEN_MULTI-1) continue;
+    else printf(",");
+  }
+  printf("\n");
+  pthread_mutex_unlock(&text_print_mutex);
+  
+  while (true) {
+    for (auto i=0; i<ARY_LEN_MULTI; ++i) {
+      auto index = id*ARY_LEN_MULTI+i;
+      if ((*dummy_array)[index]) {
+        if (rand_coin_toss()) {
+          free((*dummy_array)[index]);
+          (*dummy_array)[index] = (scNode*)malloc(sizeof(scNode));
+        }
+        else continue;
+      }
+      else (*dummy_array)[index] = (scNode*)malloc(sizeof(scNode));
+    } /* for (auto i=0; i<ARY_LEN_MULTI; ++i) */
+  }
+  pthread_exit(NULL);
+}
+
+void qt_break_malloc(size_t n_threads, QList<scNode*>& dummy_array)
+{
+  /* prepare worker parameters */
+  QtWParam** wp = (QtWParam**)malloc(sizeof(QtWParam*)*n_threads);
+  if (!wp) {
+    fprintf(stderr, "Worker paramter preparation went haywire!!\n");
+    exit(-1);
+  }
+  
+  /* Let's call the workers */
+  int rc;
+  uint64_t t;
+  pthread_t threads[n_threads];
+  
+  pthread_mutex_init(&text_print_mutex, NULL);
+  
+  for (t=0; t<n_threads; ++t) {
+    wp[t] = (QtWParam*)malloc(sizeof(QtWParam));
+    wp[t]->id = t;
+    wp[t]->array = &dummy_array;
+    rc = pthread_create(&threads[t], NULL, worker, (void*)wp[t]);
+    if (rc) {
+      fprintf(stderr, "Yikes, failed to spawn a thread!!\n");
+      exit(-1);
+    }
+  }
+  
+  pthread_mutex_destroy(&text_print_mutex);
+  pthread_exit(NULL);
+}
+
+
+
 
 #endif /* #if defined(_OPENMP) */
 
