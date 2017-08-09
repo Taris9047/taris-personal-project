ab66c39b7490593a57fe0883ebdc5eafe97d1700
Taylor Shin <talezshin@gmail.com>
2017-08-08 19:09:02 -0500

    UDP example 1
    is acting weirdly. ntohs and htons working weird.

--- UDP/ex1/client.c
+++ UDP/ex1/client.c
@@ -26,26 +26,27 @@
 int main(int argc, char* argv[])
 {
   struct sockaddr_in si_other;
-  int i, s, slen=sizeof(si_other);
+  int i, s;
+  socklen_t slen=sizeof(si_other);
   char buf[BUFLEN];
-  
+
   if ( (s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP))==-1 )
     ERROR("socket()");
-  
+
   tmemset(si_other, 0);
   si_other.sin_family = AF_INET;
   si_other.sin_port = htons(PORT);
   if (inet_aton(SRV_IP, &si_other.sin_addr)==0)
     ERROR("inet_aton()");
-  
+
   for (i=0; i<NPACK; ++i) {
     printf("Sending packet %d\n", i);
     sprintf(buf, "This is packet %d\n", i);
-    if (sendto(s, buf, BUFLEN, 0, &si_other, slen)==-1)
+    if (sendto(s, buf, BUFLEN, 0, (struct sockaddr *)&si_other, slen)==-1)
       ERROR("sendto()");
   }
-  
+
   close(s);
-  
+
   return 0;
 }
--- UDP/ex1/server.c
+++ UDP/ex1/server.c
@@ -22,7 +22,8 @@
 int main(int argc, char* argv[])
 {
   struct sockaddr_in si_me, si_other;
-  int s, slen=sizeof(si_other);
+  int s;
+  socklen_t slen=sizeof(si_other);
   char* buf = (char*)tmalloc(BUFLEN);
 
   /* Open socket */
@@ -33,11 +34,12 @@ int main(int argc, char* argv[])
   si_me.sin_family = AF_INET;
   si_me.sin_port = htons(PORT);
   si_me.sin_addr.s_addr = htonl(INADDR_ANY);
-  if ( bind(s, &si_me, sizeof(si_me)) == -1 ) ERROR("bind()");
+  if ( bind(s, (struct sockaddr *)&si_me, sizeof(si_me))==-1 ) ERROR("bind()");
 
   /* Running the server */
+  printf("Listening from Port %d\n", PORT);
   while(1) {
-    if ( recvfrom(s, buf, BUFLEN, 0, &si_other, &slen)==-1 ) ERROR("recvfrom()");
+    if ( recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *)&si_other, &slen)==-1 ) ERROR("recvfrom()");
     printf("Received packet from %s:%d\nData: %s\n\n",
       inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port), buf);
   }
