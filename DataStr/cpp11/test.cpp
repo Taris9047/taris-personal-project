9b3d333dbafead41ae45ffbc639e9edd96fba158
Taylor Shin <talezshin@gmail.com>
2018-01-18 06:56:35 -0600

    some changes

--- DataStr/cpp11/test.cpp
+++ DataStr/cpp11/test.cpp
@@ -10,22 +10,37 @@
 **********************************************/
 #include "test.hpp"
 
-int main (int argc, char* argv[])
+void test_quad_tree()
 {
-    std::cout << "Testing Quad Tree" << std::endl;
+    std::cout << "Testing Quad Tree with std::string" << std::endl;
 
     QTree<std::string> qtree;
+
+    int data_len = 5;
 
     qtree.Insert(std::string("A"), 1);
     qtree.Insert(std::string("AB"), 2);
     qtree.Insert(std::string("ABC"), 3);
+    qtree.Insert(std::string("ABCDE"), 4);
+    qtree.Insert(std::string("ABCDEF"), 5);
 
-    std::cout << "Flag 1: " << qtree.Get(1) << std::endl;
-    std::cout << "Flag 2: " << qtree.Get(2) << std::endl;
-    std::cout << "Flag 3: " << qtree.Get(3) << std::endl;
+    for (auto i=0; i<data_len; ++i)
+        std::cout << "Flag " << i+1 << ": " << qtree.Get(i+1) << std::endl;
 
+    std::cout << "Replacing Flag 2 with ABCDE" << std::endl;
     qtree.Insert(std::string("ABCDE"), 2);
-    std::cout << "Flag 2: " << qtree.Get(2) << std::endl;
+
+    std::cout << "Printing the entire quad tree again" << std::endl;
+
+    for (auto i=0; i<data_len; ++i)
+        std::cout << "Flag " << i+1 << ": " << qtree.Get(i+1) << std::endl;
+
+
+}
+
+int main (int argc, char* argv[])
+{
+    test_quad_tree();
 
 	return 0;
 }
