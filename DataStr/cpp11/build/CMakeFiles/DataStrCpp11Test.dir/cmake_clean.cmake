6911ba57d20bb151a021579a8485eac07a782dcd
Taylor Shin <talezshin@gmail.com>
2018-04-08 21:11:50 -0500

    dd

---
+++ DataStr/cpp11/build/CMakeFiles/DataStrCpp11Test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DataStrCpp11Test.dir/test.cpp.o"
+  "DataStrCpp11Test.pdb"
+  "DataStrCpp11Test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DataStrCpp11Test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
