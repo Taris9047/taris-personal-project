084d7ed602ef60f61d5137f984ce4a9011dde53b
Taylor Shin <talezshin@gmail.com>
2022-06-04 08:54:04 -0400

    "Well, it was fun anyway."

---
+++ Algorithm/Q_rsqrt/README.md
@@ -0,0 +1,16 @@
+### Result
+```shell
+Result of rsqrt 10000.00 is: 1.000000e-02
+Running 500000 repeatation.
+Time taken: 0.000001 seconds...
+
+Result of Q_rsqrt 10000.00 is: -9.983612e-03, Precision: -99.836128 % 
+Running 500000 repeatation.
+Time taken: 0.004895 seconds...
+
+Q_rsqrt is -489400.000000 % faster!!??
+```
+
+### Comment
+O boy... old codes are just old codes... After optimization, cmath is faaaaaaaaaaaaaaaaaar better!!
+
--- Algorithm/Q_rsqrt/test.c
+++ Algorithm/Q_rsqrt/test.c
@@ -2,20 +2,57 @@
 
 #include <stdio.h>
 #include <math.h>
+#include <time.h>
 
 #include "Q_rsqrt.h"
 
+float rsqrt(float number)
+{
+  float ret;
+  ret = 1.0f/sqrt(number);
+  return ret;
+}
+
 int main (void)
 {
-  float n = 100.0;
-  float ret;
-  float Q_rsqrt_ret;
+  float n = 10000.0;
+  float ret, Q_rsqrt_ret;
+  unsigned int n_repeat = 500000;
+  double time_taken_rsqrt, time_taken_Q_rsqrt;
+  float precision;
   
-  ret = 1.0/sqrt(n);
+  clock_t tm;
+
+  ret = rsqrt(n);
   printf("Result of rsqrt %.2f is: %e\n", n, ret);
+  printf("Running %d repeatation.\n", n_repeat);
+  tm = clock();
+  for (unsigned int i=0; i<n_repeat; ++i) {
+    ret = rsqrt(n);
+  }
+  tm = clock() - tm;
+  time_taken_rsqrt = ((double)tm)/CLOCKS_PER_SEC;
+  printf("Time taken: %f seconds...\n", time_taken_rsqrt);
+
+  printf("\n");
 
   Q_rsqrt_ret = Q_rsqrt(n);
-  printf("Result of Q_rsqrt %.2f is: %e\n", n, Q_rsqrt_ret);
+  precision = (1.0 - fabs(ret - Q_rsqrt_ret)/ret)*100.00f;
+  printf("Result of Q_rsqrt %.2f is: %e, Precision: %f %% \n", n, Q_rsqrt_ret, precision);
+  printf("Running %d repeatation.\n", n_repeat);
+  tm = clock();
+  for (unsigned int i=0; i<n_repeat; ++i) {
+    Q_rsqrt_ret = Q_rsqrt(n);
+  }
+  tm = clock() - tm;
+  time_taken_Q_rsqrt = ((double)tm)/CLOCKS_PER_SEC;
+  printf("Time taken: %f seconds...\n", time_taken_Q_rsqrt);
+
+  printf("\n");
+
+  float faster;
+  faster = (time_taken_rsqrt - time_taken_Q_rsqrt)/time_taken_rsqrt*100.0;
+  printf("Q_rsqrt is %f %% faster!!??\n", faster);
 
   return 0;
 }
